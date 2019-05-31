0f5b01376b2b709200d83bbfe61252d0008263bd
sdmaes18 <sdmaes@uwm.edu>
2019-05-30 22:26:54 -0500

    Created hash method in LoginTournamentForm

--- TournamentTracker/TournamentTracker/Forms/LoginTournamentForm.Designer.cs
+++ TournamentTracker/TournamentTracker/Forms/LoginTournamentForm.Designer.cs
@@ -29,32 +29,89 @@
         private void InitializeComponent()
         {
             this.LoginButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.EmailLbl = new System.Windows.Forms.Label();
+            this.PasswordLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(96, 70);
+            this.LoginButton.Location = new System.Drawing.Point(99, 173);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(221, 84);
+            this.LoginButton.Size = new System.Drawing.Size(124, 43);
             this.LoginButton.TabIndex = 0;
             this.LoginButton.Text = "Login";
             this.LoginButton.UseVisualStyleBackColor = true;
             this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(280, 173);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(126, 43);
+            this.RegisterButton.TabIndex = 1;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // EmailLbl
+            // 
+            this.EmailLbl.AutoSize = true;
+            this.EmailLbl.Location = new System.Drawing.Point(95, 46);
+            this.EmailLbl.Name = "EmailLbl";
+            this.EmailLbl.Size = new System.Drawing.Size(48, 20);
+            this.EmailLbl.TabIndex = 4;
+            this.EmailLbl.Text = "Email";
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Location = new System.Drawing.Point(95, 93);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(78, 20);
+            this.PasswordLbl.TabIndex = 5;
+            this.PasswordLbl.Text = "Password";
+            // 
             // LoginTournamentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 246);
+            this.ClientSize = new System.Drawing.Size(525, 279);
+            this.Controls.Add(this.PasswordLbl);
+            this.Controls.Add(this.EmailLbl);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RegisterButton);
             this.Controls.Add(this.LoginButton);
             this.Name = "LoginTournamentForm";
             this.Text = "LoginTournamentForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label EmailLbl;
+        private System.Windows.Forms.Label PasswordLbl;
     }
 }
--- TournamentTracker/TournamentTracker/Forms/LoginTournamentForm.cs
+++ TournamentTracker/TournamentTracker/Forms/LoginTournamentForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 using TrackerUI;
 
@@ -18,8 +20,33 @@ namespace TournamentTracker
         /// <param name="e"></param>
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            TournamentDashboardForm frm = new TournamentDashboardForm();
-            frm.Show();
+            string hashValue = this.Sha1Hash(this.PasswordLbl.Text);
+
+            MessageBox.Show(hashValue);
+
+            //TournamentDashboardForm frm = new TournamentDashboardForm();
+            //frm.Show();
+        }
+
+        /// <summary>
+        /// Provides a hash of a given string.
+        /// </summary>
+        /// <param name="text">Text to hash.</param>
+        /// <returns>Returns the hash of the given text.</returns>
+        private string Sha1Hash(string text)
+        {
+            SHA1CryptoServiceProvider sh = new SHA1CryptoServiceProvider();
+            sh.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] re = sh.Hash;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in re)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
